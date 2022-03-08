4e482d6176de2a968d402e566c99cd74cfc46a3c
Unklebens <85390097+Unklebens@users.noreply.github.com>
2022-03-08 11:54:30 +0100

    added folder existance clause

--- diskUsage.sh
+++ diskUsage.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # to be croned every week
+
+#check for folder existance
+[ ! -d /home/ubuntu/Rapports/ ] && mkdir -p /home/ubuntu/Rapports/
+
 df -h | head -n 2 | tail -n 1 | awk -F " " '{print $2" "$5}' >> /home/ubuntu/Rapports/disk_usage.log
