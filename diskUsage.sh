93e19fb853529ea6d167d1b13a029545383dabd3
Unklebens <85390097+Unklebens@users.noreply.github.com>
2022-03-10 18:50:08 +0100

    date added to output

--- diskUsage.sh
+++ diskUsage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # to be croned every week
-
 #check for folder existance
-[ ! -d /home/ubuntu/Rapports/ ] && mkdir -p /home/ubuntu/Rapports/
+[ ! -d /home/$USER/Rapports/ ] && mkdir -p /home/$USER/Rapports/
 
-df -h | head -n 2 | tail -n 1 | awk -F " " '{print $2" "$5}' >> /home/ubuntu/Rapports/disk_usage.log
+now=$(date +%Y-%m-%d)
+du=$(df -h | head -n 2 | tail -n 1 | awk -F " " '{print $2" "$5}')
+output="$now $du"
+
+echo $output >> /home/$USER/Rapports/disk_usage.log
