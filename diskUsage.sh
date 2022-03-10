169cfcfb6ff8e7f249034665154dde3149063893
Unklebens <85390097+Unklebens@users.noreply.github.com>
2022-03-10 21:35:44 +0100

    comments and weekly cron template

--- diskUsage.sh
+++ diskUsage.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# to be croned every week
-#check for folder existance
+# to be croned weekly
+#0 0 * * 0
+
+#check for folder existance and create if not
 [ ! -d /home/$USER/Rapports/ ] && mkdir -p /home/$USER/Rapports/
 
+# Get the date and format it approriately
 now=$(date +%Y-%m-%d)
+
+# Get the master volume Totral size and how much % is used
 du=$(df -h | head -n 2 | tail -n 1 | awk -F " " '{print $2" "$5}')
+
+#concatenate date and usefull data
 output="$now $du"
 
+#logging into a file
 echo $output >> /home/$USER/Rapports/disk_usage.log
